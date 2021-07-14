57f9529e2664050e2f1ce74ee1b05904352f41fe
Paul Istratoaie <p4ul.gg@gmail.com>
2021-07-14 21:56:43 +0100

    Create Quote model, serializer and migrations

--- quotes/models.py
+++ quotes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Quote(models.Model):
+
+    quote_author = models.CharField(max_length=60)
+    quote_body = models.TextField()
+    context = models.CharField(max_length=100)
+    source = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.quote_author}: {self.quote_body[:40]}..."
