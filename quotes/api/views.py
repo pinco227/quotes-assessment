708d8f8ed08665d755e34f777ca3961c84711e88
Paul Istratoaie <p4ul.gg@gmail.com>
2021-07-14 22:25:07 +0100

    Add pagination per view for quotes

--- quotes/api/views.py
+++ quotes/api/views.py
@@ -1,17 +1,20 @@
 from rest_framework import generics
 
 from quotes.models import Quote
+from .pagination import QuotesPagination
 from .permissions import IsAdminUserOrReadOnly
 from .serializers import QuoteSerializer
 
 
 class QuoteListCreateAPIView(generics.ListCreateAPIView):
-    queryset = Quote.objects.all().order_by("created_at")
+    queryset = Quote.objects.all().order_by("-created_at")
     serializer_class = QuoteSerializer
     permission_classes = [IsAdminUserOrReadOnly]
+    pagination_class = QuotesPagination
 
 
 class QuoteDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Quote.objects.all().order_by("created_at")
     serializer_class = QuoteSerializer
     permission_classes = [IsAdminUserOrReadOnly]
+    pagination_class = QuotesPagination
